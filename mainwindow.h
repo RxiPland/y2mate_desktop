4119a68ae05cc1eb61df7b71afe8ed22a63a8492
RxiPland <82058894+RxiPland@users.noreply.github.com>
2022-08-28 12:19:40 +0200

    Add files via upload

---
+++ mainwindow.h
@@ -0,0 +1,41 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QDebug>
+#include <QNetworkAccessManager>
+#include <QNetworkReply>
+#include <QNetworkRequest>
+#include <QAuthenticator>
+#include <QNetworkProxy>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+public slots:
+    void get(QString location);
+    void post(QString location, QByteArray data);
+
+
+private slots:
+    void on_pushButton_clicked();
+    void on_pushButton_2_clicked();
+
+    void on_comboBox_currentTextChanged(const QString &arg1);
+
+    void on_lineEdit_returnPressed();
+
+private:
+    Ui::MainWindow *ui;
+    QNetworkAccessManager manager;
+};
+#endif // MAINWINDOW_H
