c5b67d4ddbf8f3380908ec78388154a0f6eabadf
RxiPland <82058894+RxiPland@users.noreply.github.com>
2022-09-24 13:57:57 +0200

    Add files via upload

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -23,7 +23,7 @@ string nalezene_formaty_mp4[6] = {"nic", "nic", "nic", "nic", "nic", "nic"};
 QString nazev_souboru = "";  // název yt videa
 string cesta_k_souboru = "";
 
-QString app_version = "v1.4.2";
+QString app_version = "v1.5.0";
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
