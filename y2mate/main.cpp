2325d9765a851892282a3967363b8d0bd338e402
RxiPland <liborvanura@gmail.com>
2023-03-11 16:56:20 +0100

    added icon and variables

--- y2mate/main.cpp
+++ y2mate/main.cpp
@@ -2,10 +2,19 @@
 
 #include <QApplication>
 
+
+QString app_version = "v2.0.0";
+QByteArray user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36";
+
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    SearchVideoWindow w;
-    w.show();
+    a.setWindowIcon(QIcon(":/images/y2mate.ico"));
+    a.setQuitOnLastWindowClosed(true);
+
+    SearchVideoWindow svw;
+    svw.show();
+
     return a.exec();
 }
