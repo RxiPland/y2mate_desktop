d0e173c56e11165776f1a967c72e6284ecf29f16
RxiPland <liborvanura@gmail.com>
2023-03-12 23:35:39 +0100

    added window flags

--- y2mate/downloaddialog.cpp
+++ y2mate/downloaddialog.cpp
@@ -6,9 +6,20 @@ downloadDialog::downloadDialog(QWidget *parent) :
     ui(new Ui::downloadDialog)
 {
     ui->setupUi(this);
+    this->setWindowFlags(Qt::Dialog | Qt::WindowMinimizeButtonHint| Qt::WindowMaximizeButtonHint | Qt::MSWindowsFixedSizeDialogHint);
+    this->setWindowFlags(windowFlags() &(~Qt::WindowMinimizeButtonHint));
+    this->setWindowFlags(windowFlags() &(~Qt::WindowMaximizeButtonHint));
+
 }
 
 downloadDialog::~downloadDialog()
 {
     delete ui;
 }
+
+void downloadDialog::on_pushButton_clicked()
+{
+    downloadDialog::canceled = true;
+    this->close();
+}
+
