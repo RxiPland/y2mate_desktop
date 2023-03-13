057116bd2335e6bca26a665dd1604868fb58da63
RxiPland <liborvanura@gmail.com>
2023-03-13 20:28:03 +0100

    beutified

--- y2mate/settingsdialog.h
+++ y2mate/settingsdialog.h
@@ -21,15 +21,14 @@ public:
     bool settingsChanged = false;
 
 private slots:
-    void on_checkBox_3_clicked();
-    void on_pushButton_3_clicked();
     void on_pushButton_2_clicked();
+    void on_pushButton_3_clicked();
+    void on_pushButton_4_clicked();
     void on_checkBox_clicked();
     void on_checkBox_2_clicked();
+    void on_checkBox_3_clicked();
     void on_checkBox_4_clicked();
     void on_checkBox_5_clicked();
-
-    void on_pushButton_4_clicked();
 
 private:
     Ui::settingsDialog *ui;
